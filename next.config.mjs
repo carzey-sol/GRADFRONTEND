dad2f9a998fb0e5cdd4628434b073802f8f3a8d3
Mukesh Bajgain <112241933+carzey-sol@users.noreply.github.com>
2024-11-26 23:21:59 +0545

    second-commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    future: {
+      webpack5: true,  // Make sure Webpack 5 is being used
+    },
+  };
+  
+  export default nextConfig;
+  
