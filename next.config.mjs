b8b1ed7ff3b7d4b04dd505aefffa502c9e8a08cc
Mukesh Bajgain <112241933+carzey-sol@users.noreply.github.com>
2024-11-26 23:27:16 +0545

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    future: {
-      webpack5: true,  // Make sure Webpack 5 is being used
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
